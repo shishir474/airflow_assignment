c8caf6eafb67ed810a57bd60b0c9583bd23aca68
Shishir Kumar Singh <57223710+shishir474@users.noreply.github.com>
2023-06-08 15:34:15 +0530

    Update table_dag.py

--- dags/table_dag.py
+++ dags/table_dag.py
@@ -2,12 +2,11 @@ from airflow import DAG
 from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-# Create a DAG with a specified DAG ID, start date, schedule interval, and catch-up option
-with DAG('dag1', start_date=datetime(2022,1,1), schedule_interval='@daily', catchup=False):
-    # Task to create a table in Postgres
+with DAG('table_dag', start_date=datetime(2022,1,1), schedule_interval='@daily', catchup=False):
+    # define a task to create table using Postgres Operator
     create_table = PostgresOperator(
         task_id = 'create_table',
-        postgres_conn_id='postgres',  # Postgres connection ID
+        postgres_conn_id='postgres',
         sql='''
             CREATE TABLE IF NOT EXISTS employees(
                 first_name varchar(255) not null,
@@ -20,10 +19,10 @@ with DAG('dag1', start_date=datetime(2022,1,1), schedule_interval='@daily', catc
         '''
     )
 
-    # Task to insert values into the employees table in Postgres
+    # task to insert records 
     insert_values = PostgresOperator(
         task_id = 'insert_values',
-        postgres_conn_id='postgres',  # Postgres connection ID
+        postgres_conn_id='postgres',
         sql='''
             INSERT INTO employees (first_name, last_name, country, username, password, email)
             VALUES ('John', 'Doe', 'UK', 'johndoe10', 'pass1','johndoe@example.com'),
@@ -32,9 +31,12 @@ with DAG('dag1', start_date=datetime(2022,1,1), schedule_interval='@daily', catc
         '''
     )
 
-    # Task to retrieve data from the employees table in Postgres
+    # task to retrieve records
     retrieve_data = PostgresOperator(
         task_id = 'retrieve_data',
-        postgres_conn_id='postgres',  # Postgres connection ID
-        sql='select first_name, email from employees;'  # SQL query to retrieve data
+        postgres_conn_id='postgres',
+        sql='select first_name, email from employees;'
     )
+
+    # set the dependency between the three tasks
+    create_table >> insert_values >> retrieve_data
